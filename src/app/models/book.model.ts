b5c71443f3b6e05aa029fdb4f0522d9e73d10cc5
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-22 11:15:31 +0530

    books interface, model, schema

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interface/book.interface";
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      enum: [
+        "FICTION",
+        "NON_FICTION",
+        "SCIENCE",
+        "HISTORY",
+        "BIOGRAPHY",
+        "FANTASY",
+      ],
+      required: true,
+    },
+    isbn: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: { type: String },
+    copies: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+export const Books = model("Books", bookSchema);
